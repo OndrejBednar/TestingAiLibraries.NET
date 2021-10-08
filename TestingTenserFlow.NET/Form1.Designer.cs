5b2b8f86c03a3d5c4c70ce452e6faa13fa0f8927
Ondřej Bednář <u18.403@pslib.cloud>
2021-10-08 12:58:17 +0200

    Add project files.

---
+++ TestingTenserFlow.NET/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace TestingComputerVision.NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 249);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(222, 189);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(311, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.LoadImageClicked);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(311, 116);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Start Recording";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.StartRecordingClicked);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(593, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(195, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(593, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select video capture device";
+            // 
+            // form1BindingSource
+            // 
+            this.form1BindingSource.DataSource = typeof(TestingComputerVision.NET.Form1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "TestingComputerVision";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.BindingSource form1BindingSource;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TestingTenserFlow.NET/Form1.cs
@@ -0,0 +1,84 @@
+﻿using DirectShowLib;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TestingComputerVision.NET
+{
+    public partial class Form1 : Form
+    {
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_default.xml");
+        VideoCapture cap;
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (var device in DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice))
+            {
+                comboBox1.Items.Add(device.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void LoadImageClicked(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            var SImg = CvInvoke.Imread(dialog.FileName).ToImage<Bgr,byte>();
+            DetectFaces(SImg);
+            //CvInvoke.Resize(SImg, SImg, Size, interpolation: Emgu.CV.CvEnum.Inter.Cubic);
+            CvInvoke.Imshow("Image", SImg);
+            CvInvoke.WaitKey();
+            CvInvoke.DestroyAllWindows();
+        }
+        private void StartRecordingClicked(object sender, EventArgs e)
+        {
+            cap = new VideoCapture(comboBox1.SelectedIndex);
+            comboBox1.Enabled = false;
+            if (!cap.IsOpened)
+            {
+                return;
+            }
+            cap.ImageGrabbed += FrameCaptured;
+            cap.Start();
+        }
+
+        private void FrameCaptured(object sender, EventArgs e)
+        {
+            try
+            {
+                Mat m = new Mat();
+                cap.Retrieve(m);
+                Image<Bgr, byte> img = m.ToImage<Bgr, byte>();
+                DetectFaces(img);
+                CvInvoke.Imshow("Frame", img);
+                if (CvInvoke.WaitKey(1) == 'q')
+                {
+                    cap.Stop();
+                    cap.Dispose();
+                    comboBox1.Invoke(new Action(() => comboBox1.Enabled = true));
+                    CvInvoke.DestroyWindow("Frame");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        private void DetectFaces(Image<Bgr, byte> img)
+        {
+            Mat gray = new Mat();
+            CvInvoke.CvtColor(img, gray, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+            var faces = cascadeClassifier.DetectMultiScale(gray, 1.3, 5);
+            foreach (var face in faces)
+            {
+                img.Draw(face, new Bgr(255, 0, 0), 2);
+            }
+        }
+
+    }
+}
